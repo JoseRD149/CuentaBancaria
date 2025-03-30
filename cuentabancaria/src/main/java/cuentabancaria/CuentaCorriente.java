10ec6347c443e231e90344b56e92fe53c82bdd88
JoseRD149 <joseromerodiaz89@gmail.com>
2025-03-30 23:13:46 +0200

    changes

--- cuentabancaria/src/main/java/cuentabancaria/CuentaCorriente.java
+++ cuentabancaria/src/main/java/cuentabancaria/CuentaCorriente.java
@@ -1,5 +1,39 @@
 package cuentabancaria;
+class CuentaCorriente extends Cuenta {
+    private float sobregiro = 0;
 
-public class CuentaCorriente {
-    
+    public CuentaCorriente(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+    }
+
+    @Override
+    public void retirar(float cantidad) {
+        if (cantidad > saldo) {
+            sobregiro += (cantidad - saldo);
+            saldo = 0;
+        } else {
+            saldo -= cantidad;
+        }
+        numeroRetiros++;
+    }
+
+    @Override
+    public void consignar(float cantidad) {
+        if (sobregiro > 0) {
+            if (cantidad >= sobregiro) {
+                cantidad -= sobregiro;
+                sobregiro = 0;
+            } else {
+                sobregiro -= cantidad;
+                cantidad = 0;
+            }
+        }
+        super.consignar(cantidad);
+    }
+
+    @Override
+    public String imprimir() {
+        return super.imprimir() + ", Sobregiro: " + sobregiro;
+    }
 }
+
